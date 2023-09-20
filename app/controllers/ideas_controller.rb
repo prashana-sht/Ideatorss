3884bb79d58083e320d1622be6d0dc48e0f24746
prashana <shrestha.prashana@gmail.com>
2023-09-20 14:20:37 +0545

    Adding Content

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,4 +1,20 @@
 class IdeasController < ApplicationController
   def index
+    @ideas = Idea.all
   end
+
+  def create
+    @idea = Idea.create(description: params[:idea][:description], author: params[:idea][:author])
+    redirect_to root_path
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:description, :author)
+  end
+
+  def edit
+  end
+  
 end
