3a5c15cbcf79447cddee0c6a2fc3cba375ff7bc1
prashana <shrestha.prashana@gmail.com>
2023-09-21 14:53:30 +0545

    Adding Final Designs

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,13 +1,17 @@
 class IdeasController < ApplicationController
   def index
-    @ideas = Idea.all
+    @ideas = Idea.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
   end
 
   def create
     @idea = Idea.create(description: params[:idea][:description], author: params[:idea][:author])
+    if @idea.valid?
+      flash[:success] = "Your idea has been posted!"
+    else
+      flash[:alert] = "Woops! Looks like there has been an error!"
+    end
     redirect_to root_path
   end
-
 
   def edit
     @idea = Idea.find(params[:id])
@@ -16,26 +20,21 @@ class IdeasController < ApplicationController
   def update
     @idea = Idea.find(params[:id])
     if @idea.update(idea_params)
+      flash[:success] = "The idea has been updated!"
       redirect_to root_path
     else
+      flash[:alert] = "Woops! Looks like there has been an error!"
       redirect_to edit_idea_path(params[:id])
     end
-  end
-
-  def create
-    @idea = Idea.create(idea_params)
-    if @idea.valid?
-    else
-    end
-    redirect_to root_path
   end
 
   def destroy
     @idea = Idea.find(params[:id])
     @idea.destroy
+    flash[:success] = "The idea was susccessfully deleted!"
     redirect_to root_path
   end
-  
+
   private
 
   def idea_params
