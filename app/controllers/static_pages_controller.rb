0a2a6b60c2738a033242f351bb37a790b4fa0744
prashana <shrestha.prashana@gmail.com>
2023-09-21 13:46:24 +0545

    Static Page

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+    def about
+
+    end
+
+    def random
+        @idea = Idea.order("RAND()").first
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "ideas#index"
   resources :ideas
+  get 'about' => 'static_pages#about'
+  get 'random' => 'static_pages#random'
 end
