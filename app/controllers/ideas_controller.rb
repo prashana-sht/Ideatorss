15846416b914dd18d691878c6b794040a93e07f4
prashana <shrestha.prashana@gmail.com>
2023-09-21 12:14:56 +0545

    Updating and Deleting content

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -8,13 +8,37 @@ class IdeasController < ApplicationController
     redirect_to root_path
   end
 
+
+  def edit
+    @idea = Idea.find(params[:id])
+  end
+
+  def update
+    @idea = Idea.find(params[:id])
+    if @idea.update(idea_params)
+      redirect_to root_path
+    else
+      redirect_to edit_idea_path(params[:id])
+    end
+  end
+
+  def create
+    @idea = Idea.create(idea_params)
+    if @idea.valid?
+    else
+    end
+    redirect_to root_path
+  end
+
+  def destroy
+    @idea = Idea.find(params[:id])
+    @idea.destroy
+    redirect_to root_path
+  end
+  
   private
 
   def idea_params
     params.require(:idea).permit(:description, :author)
   end
-
-  def edit
-  end
-  
 end
